18f87bbc509ead54d527e4ab724bb4c6a48a9b38
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-10-12 01:00:17 +0200

    sm8350-common: Uprev bluetooth audio HIDL impl to 2.1
    * We get free LC3 support, isn't it poggers?
    
    Change-Id: I0f1713ee542102374183ef8088bcad62d3859c2f
    Signed-off-by: ChrisW444 <wandersonrodriguesf1@gmail.com>

--- common.mk
+++ common.mk
@@ -112,7 +112,7 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_PACKAGES += \
     android.hardware.bluetooth@1.0 \
-    android.hardware.bluetooth.audio@2.0-impl
+    android.hardware.bluetooth.audio@2.1-impl
 
 # Boot animation
 TARGET_SCREEN_HEIGHT := 2400
