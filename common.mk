36d6942406e499c3998d0ace0bc8278723450c10
kleidione Freitas <kleidione@gmail.com>
2022-03-12 15:28:57 -0300

    sm8350-common: bluetooth: Switch to full qti bt
    [1] - Fix bluetooth
    
    Signed-off-by: kleidione <kleidione@gmail.com>

--- common.mk
+++ common.mk
@@ -108,16 +108,11 @@ PRODUCT_PACKAGES += \
 # Bluetooth
 PRODUCT_PACKAGES += \
     audio.bluetooth.default \
-    libbthost_if \
-    BluetoothQti
+    libbthost_if
 
 PRODUCT_PACKAGES += \
     android.hardware.bluetooth@1.0 \
     android.hardware.bluetooth.audio@2.0-impl
-
-PRODUCT_COPY_FILES += \
-    frameworks/native/data/etc/android.hardware.bluetooth.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.bluetooth.xml \
-    frameworks/native/data/etc/android.hardware.bluetooth_le.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.bluetooth_le.xml
 
 # Boot animation
 TARGET_SCREEN_HEIGHT := 2400
