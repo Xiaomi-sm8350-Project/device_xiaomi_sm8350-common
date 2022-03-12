08efcf5c882cec1abc5bb72e5c502a96a26b8fab
kleidione Freitas <kleidione@gmail.com>
2022-03-12 18:42:35 -0300

    sm8350-common: Add GoogleCameraGo Prebuilt
    *Thanks: Greatness
    *Support Group: https://t.me/GcamGo
    
    Signed-off-by: kleidione <kleidione@gmail.com>

--- common.mk
+++ common.mk
@@ -135,7 +135,7 @@ PRODUCT_PACKAGES += \
     libgui_vendor
 
 PRODUCT_PACKAGES += \
-    Snap
+    GoogleCameraGo
 
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.camera.flash-autofocus.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.flash-autofocus.xml \
